65e316e811c9837e6d9810d88592a497875c1567
miruakasha19272 <miruakasha5@gmail.com>
2024-10-06 02:37:10 +0800

    help.js

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🇵🇭|𝗬𝗔𝗭𝗞𝗬 𝗔𝗜";
+const doNotDelete = "😸|𝗬𝗔𝗭𝗞𝗜 𝗔𝗜";
 /** 
 * @author NTKhang
 * @author: do not delete it
